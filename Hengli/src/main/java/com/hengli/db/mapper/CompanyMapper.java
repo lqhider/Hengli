78abf04bc7003aeee4d6b73e2631b1d3e67290b0
lqhider <lqhider@163.com>
2019-03-26 16:24:08 +0800

    new change

--- Hengli/src/main/java/com/hengli/db/mapper/CompanyMapper.java
+++ Hengli/src/main/java/com/hengli/db/mapper/CompanyMapper.java
@@ -10,5 +10,7 @@ public interface CompanyMapper {
 	
 	public List<Map<String,Object>> selectCompany(Map<String, Object> params);
 	
+	public List<Map<String,Object>> selectCompanyNoLongitudeLatitude();
+	
 	public List<Map<String,Object>> selectCompanyDistribution(Map<String, Object> params);
 }
