9e5b2a1f65851d10b28b86faea939b915f802bbc
lqhider <lqhider@163.com>
2019-03-26 15:52:57 +0800

    new change

---
+++ Hengli/src/main/java/com/hengli/controller/ToolsController.java
@@ -0,0 +1,39 @@
+package com.hengli.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hengli.db.mapper.CollegesMapper;
+import com.hengli.db.mapper.CompanyMapper;
+import com.hengli.util.Utils;
+
+
+@Controller
+@RequestMapping("/tools")
+public class ToolsController {
+	
+	@Autowired
+	public CompanyMapper companyMapper;
+	
+	@Autowired
+	public CollegesMapper collegesMapper;
+	
+	/**
+	 * 获取企业,院校,设计公司经纬度
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/getLongitudeLatitude")
+	@ResponseBody
+	public Map<String, Object> getLongitudeLatitude(){
+		
+		
+		
+		return Utils.returnResult(null);
+	}
+	
+}
