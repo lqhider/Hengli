06e3690e561b434a0681c08d193264ffb76bb0be
lqhider <lqhider@163.com>
2019-03-27 21:10:42 +0800

    new change

--- Hengli/src/main/webapp/resources/js/view/gx.js
+++ Hengli/src/main/webapp/resources/js/view/gx.js
@@ -12,5 +12,22 @@
         })();
 
         $(window).resize(resizePage);
+        
+        //下拉列表初始化滚动条
+        $(".comp-list").niceScroll({
+            zindex:4,
+            cursorcolor: "#0099ff",
+            autohidemode: false,
+            cursorborder: "1px solid transparent",
+            cursorwidth: "8px"
+        });
+      //下拉列表初始化滚动条
+        $(".center").niceScroll({
+            zindex:4,
+            cursorcolor: "#0099ff",
+            autohidemode: false,
+            cursorborder: "1px solid transparent",
+            cursorwidth: "4px"
+        });
 	})
 })(jQuery)
